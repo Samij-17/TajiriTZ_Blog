0790ec9530ae336a1d10818e4e7f9947db60085c
Samij-17 <116192573+Samij-17@users.noreply.github.com>
2023-08-25 09:48:04 +0300

    Updates

---
+++ precache-manifest.25041dea9106d2129bafda49d65fab62.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "a968c1d366ef8b096bd5",
+    "url": "/TajiriTZ_Blog/static/js/main.a968c1d3.chunk.js"
+  },
+  {
+    "revision": "cfd0219adc4002ed8c3c",
+    "url": "/TajiriTZ_Blog/static/css/1.78f3a437.chunk.css"
+  },
+  {
+    "revision": "cfd0219adc4002ed8c3c",
+    "url": "/TajiriTZ_Blog/static/js/1.cfd0219a.chunk.js"
+  },
+  {
+    "revision": "4d04e430774329519c25",
+    "url": "/TajiriTZ_Blog/static/js/runtime~main.4d04e430.js"
+  },
+  {
+    "revision": "979733b2effd10bc088b7ee4800fba71",
+    "url": "/TajiriTZ_Blog/index.html"
+  }
+];
